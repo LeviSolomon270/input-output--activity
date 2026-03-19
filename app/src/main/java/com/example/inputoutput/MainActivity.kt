e10616d1828f236c8cc567b8ff2dab4a0a01bd9f
Levi <solomonlevi270@gmail.com>
2026-03-19 16:23:00 +0200

    language

--- app/src/main/java/com/example/inputoutput/MainActivity.kt
+++ app/src/main/java/com/example/inputoutput/MainActivity.kt
@@ -8,7 +8,6 @@ import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Switch
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -32,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         val displayText = findViewById<TextView>(R.id.Displaytext)
         val nameText = findViewById<EditText>(R.id.NameText)
         val zuluSwitch = findViewById<Switch>(R.id.zuluSwitch)
-        val langaugeSpinner = findViewById<Spinner>(R.id.Lamguages)
+        val langaugeSpinner = findViewById<Spinner>(R.id.Languages)
         langaugeSpinner.adapter = ArrayAdapter(
             MainActivity, android.R.layout.simple_spinner_item,
             languages.values()
